7695bb5eaf9708c25052214fc93fa2ee641b6762
Vuk Vukovic <vuk.vukovic.2000@gmail.com>
2024-05-25 18:28:54 +0200

    Project setup (#2)
    * feat: application.properties and gradle setup
    
    This configures application properties and gradle file and dependencies
    
    * feat: spring security
    
    * feat: exceptions and exception handling
    
    * feat: implemented auth/jwt utils
    
    This allows to extract data from security context holder

---
+++ src/main/java/ftn/notificationservice/exception/ExceptionMessage.java
@@ -0,0 +1,12 @@
+package ftn.notificationservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionMessage {
+
+    private String message;
+
+}
